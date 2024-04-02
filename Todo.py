ee9866e58e0d9e6a40de23056bba9c36f7a73faa
Lean Kurt <leankurtescano@gmail.com>
2024-04-02 23:08:02 +0800

    constructed main program

--- Todo.py
+++ Todo.py
@@ -5,7 +5,7 @@ def addTask():
     taskNo = 0  
     while not exit == 1:
         newTask = input("Add new Task: \n")
-        my_task = {"Task": newTask, "Done Task": False, "Number": taskNo}
+        my_task = {"Task": newTask, "Done Task": False, "taskNumber": taskNo}
         task.append(my_task)
         taskNo += 1
         print(task)
@@ -32,16 +32,23 @@ def deleteTask(task):
             exit = 1
       else:
             print("You have entered an invalid input")
-      
-      
-     
             
 def mainProgram(task):
     exit = 0
     while not exit == 1:
-      addTask()
-      deleteTask(task)
-      ifExit = input("Do you want to continue")
+      print("Welcome to your Todo list manager in python\n")
+      print("Select from the options")
+      print("1. Add task")
+      print("2. Delete task \n")
+      option = str(input("Choose a number base from your option(1-2):"))
+      if option == "1":
+            addTask()
+      elif option == "2":
+            deleteTask(task)
+      else:
+            print("Invalid option. Please select 1 or 2.")
+            continue
+      ifExit = input("Do you want to continue:")
       if ifExit == "yes" or ifExit == "y": 
            continue
       elif ifExit == "no" or ifExit == "n":  # Use logical OR here
